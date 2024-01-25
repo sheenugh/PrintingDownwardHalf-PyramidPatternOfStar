bd4825a2c196c2c48cfbfee03adcfb82bc2ec6d7
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-25 12:56:25 +0800

    sa wakas nagrun na sya : ). nakakastress

--- printing_downward_half-pyramid_using_asterisk.py
+++ printing_downward_half-pyramid_using_asterisk.py
@@ -1,6 +1,25 @@
 
+# ----- VISUALIZATION -----
+# * * * * *                         1*2 - 1, 1 | 0, 4 --> #height-i | 0 spaces on the left, 4 spaces on the right
+# * * * *                           2*2 - 1, 3 | 0, 3 --> #height-i | 0 spaces on the left, 3 spaces on the right
+# * * *                             3*2 - 1, 5 | 0, 2 --> #height-i | 0 spaces on the left, 2 spaces on the right
+# * *                               4*2 - 1, 7 | 0, 1 --> #height-i | 0 spaces on the left, 1 spaces on the right
+# *                                 5*2 - 1, 9 | 0, 0 --> #height-i | 0 spaces on the left, 0 spaces on the right
+
+# n or height = 5
+# number of index = prev index/variable + 1 
+
+
 # ----- FUNCTIONS -----
 
 # ----- PSEUDO CODE -----
 # - ACTUAL CODE
 # - 
+
+def downward_half_pyramid(height):
+    for i in range(height, 0, -1):
+        stars = '* ' * (i - 1)
+        print(stars)
+
+# Calling the function with 5 rows
+downward_half_pyramid(6)
---
+++ trying_on_my_own.py
@@ -0,0 +1,25 @@
+
+# ----- VISUALIZATION -----
+# * * * * *                         5*1 - 0, 5 | 0, 4 --> #height-i | 0 spaces on the left, 4 spaces on the right
+# * * * *                           4*1 - 0, 4 | 0, 3 --> #height-i | 0 spaces on the left, 3 spaces on the right
+# * * *                             3*1 - 0, 3 | 0, 2 --> #height-i | 0 spaces on the left, 2 spaces on the right
+# * *                               2*1 - 0, 2 | 0, 1 --> #height-i | 0 spaces on the left, 1 spaces on the right
+# *                                 1*1 - 0, 1 | 0, 0 --> #height-i | 0 spaces on the left, 0 spaces on the right
+
+# n or height = 5
+# number of index = prev index/variable + 1 
+
+
+# ----- FUNCTIONS -----
+def right_triangle_using_num(height):
+    for i in range(height, 0, -1):
+        print(("*" + " ") * ((i*1)-0), end= " ")
+        print(" " * (height - i))
+
+# PSEUDO CODES
+# - Actual codes
+# - Calling the function
+right_triangle_using_num(5)
+
+
+# ------------------------
