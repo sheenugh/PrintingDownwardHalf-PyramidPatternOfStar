0e1b64935feda2f4408500e1468757192c031fa3
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-25 12:58:19 +0800

    changes in visualization

--- printing_downward_half-pyramid_using_asterisk.py
+++ printing_downward_half-pyramid_using_asterisk.py
@@ -1,10 +1,10 @@
 
 # ----- VISUALIZATION -----
-# * * * * *                         5*1 - 0, 5 | 0, 4 --> #height-i | 0 spaces on the left, 4 spaces on the right
-# * * * *                           4*1 - 0, 4 | 0, 3 --> #height-i | 0 spaces on the left, 3 spaces on the right
+# * * * * *                         5*1 - 0, 5 | 0, 4 --> #height-i | 0 spaces on the left, 0 spaces on the right
+# * * * *                           4*1 - 0, 4 | 0, 3 --> #height-i | 0 spaces on the left, 1 spaces on the right
 # * * *                             3*1 - 0, 3 | 0, 2 --> #height-i | 0 spaces on the left, 2 spaces on the right
-# * *                               2*1 - 0, 2 | 0, 1 --> #height-i | 0 spaces on the left, 1 spaces on the right
-# *                                 1*1 - 0, 1 | 0, 0 --> #height-i | 0 spaces on the left, 0 spaces on the right
+# * *                               2*1 - 0, 2 | 0, 1 --> #height-i | 0 spaces on the left, 3 spaces on the right
+# *                                 1*1 - 0, 1 | 0, 0 --> #height-i | 0 spaces on the left, 4 spaces on the right
 
 
 # ----- FUNCTIONS -----
